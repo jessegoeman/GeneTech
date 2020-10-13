a0fbd1fc9ac9be3a353b61bd759cb9f74c1a5cc0
jesse <jessegoeman@gmail.com>
2020-10-13 17:44:14 +0200

    improved logic

--- ChainConverter.py
+++ ChainConverter.py
@@ -1,3 +1,5 @@
+#COPY YOUR CHAIN IN ChainInput.txt, more info about the markup in bespoke file
+
 with open("ChainInput.txt","r+") as r:
     sequence = r.readline().strip("\n")
 
@@ -6,20 +8,20 @@ sequenceNew = ""
 
 seq = "".join(dictNuc.get(char.upper(),char) for char in sequence)
 
+startsign,stopsign = "5'","3'"
+origineel = f'{startsign}-{sequence.upper()}-{stopsign}'
+oplossing = f'{stopsign}-{seq}-{startsign}'
+
 temp = 0
 primer_parts = seq.split("-")
 if len(primer_parts[0])>len(primer_parts[1]):
     total_temp = sum([temp+4 if codon in "GC" else 2 for codon in primer_parts[0]])
+    primer = "REVERSE PRIMER: " + oplossing[::-1] + " temp: " + str(total_temp)
 else:
     total_temp = sum([temp + 4 if codon in "GC" else 2 for codon in primer_parts[2]])
+    primer = "FORWARD PRIMER: " + origineel + " temp: " + str(total_temp)
 
-
-startsign,stopsign = "5'","3'"
-oplossing = f'{stopsign}-{seq}-{startsign}'
-
-print('-'*10)
-print(f'{startsign}-{sequence.upper()}-{stopsign}')
-print(oplossing)
-print('-'*10)
-print("FORWARD PRIMER: " +oplossing + " temp: "+str(total_temp))
-print("REVERSE PRIMER: "+ oplossing[::-1] + " temp: "+str(total_temp))
+print('-'*30)
+print(origineel+"\n"+oplossing)
+print('-'*30)
+print(primer)
